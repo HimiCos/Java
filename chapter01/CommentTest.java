358dd402449627f81b53676708e55d0858ea6f23
HimiCos <ineewhite@gmail.com>
2023-09-08 17:19:10 +0800

    day: 1

---
+++ chapter01/CommentTest.java
@@ -0,0 +1,18 @@
+/**
+ * 這是我的第一個Java程式（文檔注釋）
+ * @author HimiCos
+ * @version 1.0
+ * @since 2023-09-08
+ */
+public class CommentTest {
+  public static void main(String[] args) {
+    // System.out.println("こんにちは、世界!!");
+    System.out.print("こんにちは、世界!!");
+    /*
+     * System.out.println("こんにちは、世界!!");
+     */
+    /**
+     * 這是我的第一個Java程式（文檔注釋）
+     */
+  }
+}
---
+++ chapter01/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloJapan {
+  public static void main(String[] args) {
+    System.out.println("こんにちは、世界!!");
+    System.out.print("こんにちは、世界!!");
+    System.out.println("こんにちは、世界!!");
+    System.out.print("123");
+    System.out.println(123 + 1);
+  }
+}
